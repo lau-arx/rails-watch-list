8a552562675d9850580bff98bab30a0471dadc3a
Laura Auroux <laura.auroux@live.fr>
2021-05-13 23:00:54 +0200

    with style

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,40 @@
+class ListsController < ApplicationController
+  before_action :find_list, only: [ :show ]
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+  
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save 
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private 
+
+  def find_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
